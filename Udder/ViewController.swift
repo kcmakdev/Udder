0bd7f333da3dfc74538fe41bc19ea70598adb9a9
makkacheong <gaku113@hotmail.com>
2019-05-21 09:23:20 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by KC Mak on 21/5/2019.
+//  Copyright © 2019 KC Mak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
